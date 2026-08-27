314969aa9d38f5ecb62653a0cfd178b439ee070b
deeepbreath <kay@linux.do>
2026-08-27 12:02:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,7 +10,13 @@
 # 克隆自定义包
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic || true
 git clone --depth=1 https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman.git package/luci-app-podman || true
-git clone --depth=1 https://github.com/QiuSimons/luci-app-daed || true
+git clone --depth=1 https://github.com/QiuSimons/luci-app-daed package/luci-app-daed || true
+git clone --depth=1 https://github.com/kenzok8/openwrt-daede package/openwrt-daede || true
+git clone --depth=1 https://istore.istoreos.com/repo-apk/all/store package/luci-app-store || true
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon || true
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-app-argon-config || true
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-i18n-argon-config-zh-cn || true
+
 
 # 安装 feeds
 ./scripts/feeds install -a
